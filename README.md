fb17abae539d4997f57f345e32a2e75de445f4b3
Maxi Ferreira <charca@gmail.com>
2024-06-04 00:43:28 -0700

    Initial implementation

---
+++ README.md
@@ -0,0 +1,12 @@
+# DOM Depth Test
+
+Read the article 👉🏽 https://www.frontendatscale.com/blog/how-deep-is-your-dom
+
+1. `npm install`
+2. `npm start`
+
+Query params available:
+
+- `elements` (int): number of elements to render
+- `nested` (bool): whether to nest the elements or not
+- `css` (bool): whether to apply a CSS rule or not
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,55 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+function generateBalancedDomTree(numElements: number) {
+	let html = '';
+	for (let i = 0; i < numElements; i++) {
+		if (i === numElements - 1) {
+			html += `<div id="last-div">👉🏽 This is the last of ${numElements} divs.</div>`;
+		} else {
+			html += `<div></div>`;
+		}
+	}
+	return html;
+}
+
+function generateUnbalancedDomTree(numElements: number) {
+	function createElement(remaining: number): string {
+		if (remaining === 0) return `<div id="last-div">👉🏽 This is the last of ${numElements} divs.</div>`;
+
+		return `<div>${createElement(remaining - 1)}</div>`;
+	}
+
+	return createElement(numElements);
+}
+
+function generateStyleTag() {
+	return `
+		<style>
+			div {
+				padding: 10px 0;
+			}
+		</style>
+	`;
+}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		const numElements = parseInt(url.searchParams.get('elements') || '') || 1_000;
+		const isNested = url.searchParams.get('nested') === 'true';
+		const withCSS = url.searchParams.get('css') === 'true';
+
+		const html = `
+
+		<h1>DOM Depth Test</h1>
+		<p>DOM Elements: ${numElements}</p>
+		<p>DOM Depth: ${isNested ? numElements : 1}</p>
+		${isNested ? generateUnbalancedDomTree(numElements) : generateBalancedDomTree(numElements)}
+		${withCSS ? generateStyleTag() : ''}
+		`;
+
+		return new Response(html, {
+			headers: {
+				'content-type': 'text/html;charset=UTF-8',
+			},
+		});
 	},
 };
